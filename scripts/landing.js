8293c4a3fb0db9adc8d12138e5bf034bebb16363
Martina Vargas <martinavargass04@gmail.com>
2023-06-02 23:43:35 -0300

    landing finished

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,74 @@
-const openModal = document.querySelector('.name')
-const modal = document.querySelector('dialog')
-const buttonClose = document.querySelector('.modal-close')
-const buttonAccept = document.querySelector('.modal-accept')
-const text = document.querySelector('.game-story')
-const buttons = document.querySelector('.buttons')
+const openModal = document.querySelector(".button-name");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector(".modal-close");
+const buttonAccept = document.querySelector(".modal-accept");
+const text = document.querySelector(".game-story");
+const buttons = document.querySelector(".buttons");
+const nameField = document.querySelector("[name=name]");
+const buttonStart = document.querySelector(".button-start");
+const form = document.querySelector("form");
+const footer = document.querySelector("footer");
 
-openModal.addEventListener('click', (e) => {
+document.addEventListener("DOMContentLoaded", function () {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  openModal.addEventListener("click", (e) => {
     modal.showModal();
-    text.classList.add('dissapear')
-    buttons.classList.add('dissapear')
-    modal.classList.add('open');
-})
+    text.classList.add("dissapear");
+    buttons.classList.add("dissapear");
+    footer.classList.add("dissapear");
+    modal.classList.add("open");
+  });
 
-buttonClose.addEventListener('click', (e) => {
+  function closeModal() {
     modal.close();
-    text.classList.remove('dissapear')
-    buttons.classList.remove('dissapear')
-    modal.classList.remove('open');
-})
+    text.classList.remove("dissapear");
+    buttons.classList.remove("dissapear");
+    footer.classList.remove("dissapear");
+    modal.classList.remove("open");
+    nameField.classList.remove("invalid");
+    nameField.nextElementSibling.classList.remove("error");
+    nameField.nextElementSibling.innerText = "";
+  }
 
-buttonAccept.addEventListener('click', (e) => {
-    modal.close();
-    text.classList.remove('dissapear')
-    buttons.classList.remove('dissapear')
-    modal.classList.remove('open');
-})
+  buttonAccept.disabled = true;
 
-function toggleNavbar(){
-    const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('show');
-}
+  function validation() {
+    const field = nameField;
+    const fieldValue = nameField.value.trim();
+    if (fieldValue.trim().length === 0) {
+      field.classList.add("invalid");
+      field.nextElementSibling.classList.add("error");
+      field.nextElementSibling.innerText = "Debes escribir un nombre.";
+      buttonAccept.disabled = true;
+    } else {
+      field.classList.remove("invalid");
+      field.nextElementSibling.classList.remove("error");
+      field.nextElementSibling.innerText = "";
+      buttonAccept.disabled = false;
+    }
+  }
+
+  buttonAccept.addEventListener("click", (e) => {
+    let name = nameField.value.trim();
+    localStorage.setItem("name", name);
+    const playerName = localStorage.getItem("name");
+  });
+
+  buttonStart.addEventListener("click", (e) => {
+    if (!buttonAccept.disabled) {
+      window.location.href = "../pages/game.html";
+    }
+  });
+
+  buttonAccept.onclick = () => {
+    buttonStart.classList.add("button-start-enabled");
+    buttonStart.style.cursor = "pointer";
+  };
+
+  buttonClose.addEventListener("click", closeModal);
+  buttonAccept.addEventListener("click", closeModal);
+  nameField.addEventListener("input", validation);
+});
