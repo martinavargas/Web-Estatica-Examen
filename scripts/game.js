7538941d842738300a4b48eab96346f5a4e06193
Nicolas Ruiz <nicolas_8585@hotmail.com>
2023-06-09 17:47:49 -0300

    Organización de todo y se agregó scores

---
+++ scripts/game.js
@@ -0,0 +1,159 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const myShip = document.querySelector('img[name="ship"]');
+  const buttonsWrapper = document.querySelector(".wrapper");
+  const leftButton = document.getElementById("left");
+  const rightButton = document.getElementById("right");
+  const fireButton = document.getElementById("fire");
+
+  let projectileVelocity = 10;
+  let bulletPeriod = 100;
+
+  leftButton.textContent = "<";
+  leftButton.style.fontSize = "1rem";
+  rightButton.textContent = ">";
+  rightButton.style.fontSize = "1rem";
+
+  myShip.style.transform = "scaleX(2rem) scaleY(2rem)";
+
+  let originalPlace = window.innerWidth / 2 - myShip.offsetWidth / 2;
+  const movementStep = 5;
+
+  const moveShip = () => {
+    myShip.style.transform = `translateX(${originalPlace}px)`;
+  };
+
+  leftButton.onclick = () => {
+    originalPlace -= movementStep;
+
+    // Limitar el movimiento dentro de los límites de la pantalla
+    const minPositionX = 0;
+    originalPlace = Math.max(minPositionX, originalPlace);
+
+    moveShip();
+  };
+
+  rightButton.onclick = () => {
+    originalPlace += movementStep;
+
+    // Limitar el movimiento dentro de los límites de la pantalla
+    const maxPositionX = window.innerWidth - myShip.offsetWidth;
+    originalPlace = Math.min(originalPlace, maxPositionX);
+
+    moveShip();
+  };
+
+  buttonsWrapper.classList.add("hidden");
+
+  window.addEventListener("keydown", (event) => {
+    const { key } = event;
+
+    if (key === "ArrowLeft") {
+      originalPlace -= movementStep;
+    }
+    if (key === "ArrowRight") {
+      originalPlace += movementStep;
+    }
+
+    // Limitar el movimiento dentro de los límites de la pantalla
+    const maxPositionX = window.innerWidth - myShip.offsetWidth;
+    const minPositionX = 0;
+    originalPlace = Math.max(
+      minPositionX,
+      Math.min(originalPlace, maxPositionX)
+    );
+
+    moveShip();
+  });
+
+  moveShip();
+
+  const createProjectile = () => {
+    const projectile = document.createElement("img");
+    projectile.setAttribute("src", "/assets/fx/bullet.svg");
+    projectile.classList.add("projectile");
+
+    const shipPosition = myShip.getBoundingClientRect();
+    const projectilePosition = {
+      x: shipPosition.left,
+      y: shipPosition.top + shipPosition.width / 2,
+    };
+
+    projectile.style.left = `${projectilePosition.x}px`;
+    projectile.style.top = `${projectilePosition.y}px`;
+
+    document.body.appendChild(projectile);
+
+    return projectile;
+  };
+
+  const isCollision = (projectile, enemy) => {
+    const rectProjectile = projectile.getBoundingClientRect();
+    const rectEnemy = enemy.getBoundingClientRect();
+
+    const collision =
+      rectProjectile.left < rectEnemy.right &&
+      rectProjectile.right > rectEnemy.left &&
+      rectProjectile.top < rectEnemy.bottom &&
+      rectProjectile.bottom > rectEnemy.top;
+
+    if (collision) {
+      const collisionSound = document.getElementById("collisionSound");
+      collisionSound.play();
+    }
+
+    return collision;
+  };
+
+  const moveProjectile = (projectile) => {
+    return setInterval(() => {
+      projectile.style.top = `${projectile.offsetTop - projectileVelocity}px`;
+
+      // Verificar si el proyectil colisiona con alguna nave enemiga
+      const enemies = document.querySelectorAll(".enemy-ship");
+      enemies.forEach((enemy) => {
+        if (!enemy.classList.contains("destroyed")) {
+          if (isCollision(projectile, enemy)) {
+            enemy.classList.add("destroyed");
+            projectile.remove();
+            sumarPuntos(5);
+          }
+        }
+      });
+
+      // Verificar si el proyectil ha salido de la pantalla
+      if (projectile.offsetTop < 0) {
+        projectile.remove();
+        clearInterval();
+      }
+    }, bulletPeriod);
+  };
+
+  const shoot = () => {
+    const projectile = createProjectile();
+    const enemies = document.querySelectorAll(".enemy-ship");
+
+    moveProjectile(projectile, enemies);
+    const shotSound = new Audio("/assets/sounds/laser-shoot.mp3");
+    shotSound.play();
+  };
+
+  document.addEventListener("keydown", (event) => {
+    const { key } = event;
+    if (key === " ") {
+      shoot();
+    }
+  });
+
+  fireButton.addEventListener("click", () => {
+    shoot();
+  });
+});
+
+let score = 0;
+
+/* funcion para los puntos */
+function sumarPuntos(cantidad) {
+  score += cantidad;
+  const scoreValueElement = document.getElementById("score-value");
+  scoreValueElement.textContent = score.toString().padStart(4, '0');
+} 
